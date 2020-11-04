654306b4770b2a85811c754d364687cf58f26a24
Ruth <ruths@indiefin.com>
2020-11-04 17:03:39 +0200

    Update pages/index.js
    Fetching data from API

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
-import React from "react"
+import React, { useState, useEffect} from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+
+  const [transactions, setTransaction] = useState([])
+
+  useEffect(() => {
+    fetch('https://indie-transaction-api.netlify.app//.netlify/functions/api/api')
+    .then((res) => res.json())
+    .then((data) => setTransaction(data.transactions))
+  }, [setTransaction])
+
+  return <div>{JSON.stringify(transactions, null, 2)}</div>
 }
