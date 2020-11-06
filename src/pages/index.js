b86f792018bacade3fb1f1f2d48afdf7a99555dc
leratorabotapi <leratorabotapi@hotmail.com>
2020-11-06 13:06:09 +0200

    localStorage fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,22 +8,30 @@ export default function Home() {
   const [transactions, setTransaction] = useState()
 
 
-  // Save to localStorage
   useEffect(() => {
-    localStorage.setItem('accountData', JSON.stringify(transactions));
-  })
+
+    // Get the existing data
+    var existing = localStorage.getItem('accountData');
+
+    // If no existing data, use the value by itself
+    // Otherwise, add the new value to it
+    var data = existing ? existing + transactions : transactions;
+
+    // Save back to localStorage
+    localStorage.setItem('accountData', data );
+    
+    })
+
 
   useEffect(() => {
     fetch('https://indie-transaction-api.netlify.app//.netlify/functions/api/api')
     .then((res) => res.json())
     .then((data) => setTransaction(data.transactions))
-  }, [setTransaction])
+  }, [])
 
   return <div className="App">
     <ApplicationContext.Provider
-    value= {{
-      
-    }}>
+    value= {{ }}>
     <h1>Transactions</h1>
 
     <pre>{JSON.stringify(transactions, null, 2)}</pre>
