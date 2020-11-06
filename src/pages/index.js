fde2f06149644487434a67839aa536353031e355
leratorabotapi <leratorabotapi@hotmail.com>
2020-11-06 17:36:01 +0200

    Show Income/CSS grid added
    You can now see income, in addition to expenses
    CSS grid temporarily added to better display JSON data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect} from "react"
 import axios from "axios";
+import './style.css'
 
 import ApplicationContext from '../components/ApplicationContext/Application';
 
@@ -30,6 +31,8 @@ export default function Home() {
       });
   };
 
+   // Getting sum of numbers
+
   useEffect(() => {
     console.log("run once");
     if (!isFetching) {
@@ -42,7 +45,8 @@ export default function Home() {
   return <div className="App">
     <ApplicationContext.Provider
     value= {{ }}>
-
+    
+    <div>
      <h1>data from state</h1>
       <button
         onClick={() => {
@@ -52,21 +56,43 @@ export default function Home() {
       >
         clear localStorage
       </button>
+
       <button disabled={isFetching} onClick={() => fetchData()}>
         fetch new data
-      </button>      <p>items in transaction history {transactions.length}</p>
-      <pre>{JSON.stringify(transactions, null, 2)}</pre>
+      </button>
 
+      <p>items in transaction history {transactions.length}</p>
+
+      <pre>{JSON.stringify(transactions, null, 2)}</pre>
+    </div>
+
+    <div>
       <h2>Expenses</h2>
+
       {transactions
       .filter(expenses => expenses.amount < 0 )
-      .map(expenses => (<pre>{JSON.stringify(expenses, null, 2)}</pre>))}
+      .map(expenses => (
 
-      <h2>Income</h2>
-      {transactions
+      <div>
+        <pre>{JSON.stringify(expenses, null, 2)}</pre>
+      </div>
+
+      ))}
+    </div>
+
+    <div>
+<h2>Income</h2>
+{transactions
       .filter(income => income.amount > 0 )
-      .map(income => (<pre>{JSON.stringify(income, null, 2)}</pre>))}
+      .map(income => (
+
+      <div>
+        <pre>{JSON.stringify(income, null, 2)}</pre>
+      </div>
+      ))}
+    </div>
       
+
     </ApplicationContext.Provider>
     </div>
 }
