b0a3e74de5d4762ca03d2e660c31fea84af7c477
Ruth <ruths@indiefin.com>
2020-11-18 08:26:30 +0200

    Reports page
    Adding charts with dummy data
    Made adjustments to index.js, there was a lot of errors

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,38 +1,39 @@
-import React, { useContext } from "react"
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
 import ApplicationContext from '../ApplicationContext/Application'
-import { Button } from "../Button/Button";
-import Copy from "../Copy";
-import "./style.css"
+// import { Button } from "../Button/Button";
+import { Button } from '@indiefin/galaxy-button'
+import Copy from '../Copy'
+import './style.css'
 
 const Modal = ({ title, children }) => {
   const { showModal, closeModal } = useContext(ApplicationContext)
 
   return (
-      <div className={showModal ? "overlay" : "hide"} onClick={closeModal}>
-        <div className={showModal ? "modal" : "hide"}>
+      <div className={showModal ? 'overlay' : 'hide'} onClick={closeModal}>
+        <div className={showModal ? 'modal' : 'hide'}>
             <div className="header">
-               <Copy fontType="H4" copyColor="black" weight="light">{title}</Copy> 
+               <Copy fontType="H4" copyColor="black" weight="light">{title}</Copy>
             </div>
             <div className="body">
                 <Copy>{children}</Copy>
             </div>
             <div className="buttons">
-                <Button onClick={closeModal} primary={false} label="Cancel" backgroundColor="grey" ></Button>
-                <Button onClick={closeModal} primary={true} label="Save Changes" backgroundColor="hotpink" ></Button> 
-            </div>     
+                <Button handleClick={closeModal} color="blue" size="regular" kind="filled">Save Changes</Button>
+                <Button handleClick={closeModal} kind="outline">Cancel</Button>
+            </div>
         </div>
       </div>
-    
+
   )
 }
 
 Modal.propTypes = {
-    children: PropTypes.oneOfType([
-        PropTypes.arrayOf(PropTypes.node),
-        PropTypes.node
-      ]).isRequired,
-    title: PropTypes.string.isRequired
-  }
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired,
+  title: PropTypes.string.isRequired
+}
 
 export default Modal
--- src/pages/reports.js
+++ src/pages/reports.js
@@ -2,20 +2,270 @@ import React from 'react'
 import './style.css'
 import Copy from '../components/Copy'
 import Layout from '../components/layout/Layout'
-import Content from '../components/Content'
-import './style.css'
-import {  Row } from 'antd'
-import 'antd/dist/antd.css'
+import { Row, Col } from 'antd'
+import { Line, HorizontalBar, Doughnut, Bar } from 'react-chartjs-2'
+import Content from '../components/Content';
 
-export default function Reports() {
-    return (
-        <Layout>
-            <Content />
-            <div className="App">
-            <Row className="title">
-            <Copy fontType="H1" weight="bold">Reports</Copy>
-            </Row>
+export default function Reports () {
+  const lineData = {
+    labels: ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'],
+    datasets: [
+      {
+        label: 'Expenditure',
+        fill: false,
+        lineTension: 0,
+        backgroundColor: 'rgba(0,0,0,1)',
+        borderColor: 'rgba(0,0,0,1)',
+        borderWidth: 2,
+        data: [65, 59, 80, 81, 56, 81, 84]
+      }
+    ]
+  }
+
+  const horiData = {
+    labels: ['Transport', 'Shopping', 'Education', 'Food & Drink'],
+    datasets: [
+      {
+        backgroundColor: '#4483CA',
+        barThickness: 9,
+        data: [65, 59, 80, 81]
+      }
+    ]
+  }
+
+  const donutData = {
+    labels: ['Transport', 'Shopping', 'Education', 'Food & Drink'],
+    datasets: [
+      {
+        backgroundColor: [
+          '#76C2B6',
+          '#00194D',
+          '#0151A3',
+          '#2E8ED3'
+        ],
+        hoverBackgroundColor: [
+          '#76C2B6',
+          '#00194D',
+          '#0151A3',
+          '#2E8ED3'
+        ],
+        data: [65, 59, 80, 81]
+      }
+    ]
+  }
+
+  const yearData = {
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
+    datasets: [
+      {
+        label: 'Income',
+        data: [3000, 2800, 2700, 2750, 3000, 2800, 2700, 2750, 3000, 2800, 2700, 2750],
+        backgroundColor: '#76C2B6',
+        borderColor: '#6610f2',
+        barThickness: 9
+      },
+      {
+        label: 'Expenses',
+        data: [2550, 2710, 2700, 2750, 2000, 2550, 2710, 2700, 2750, 2000, 1700, 1750],
+        backgroundColor: '#00194D',
+        borderColor: '#E0E0E0',
+        barThickness: 9
+      }
+    ]
+  }
+
+  const progressbar = {
+    labels: false,
+    datasets: [
+      {
+        backgroundColor: '#4483CA',
+        barThickness: 9,
+        data: [3, 6]
+      }
+    ]
+  }
+  return (
+    <Layout name="reports">
+      <Content />
+      <div className="App">
+      <Row style={{ paddingTop: '45px' }}>
+            <Col span={12}>
+            <div className="reportDiv1">
+              <Copy fontType="H1" weight="bold">Reports</Copy>
             </div>
-            </Layout>
-    )
+            </Col>
+
+            <Col span={12}>
+              <div className="reportDiv2">
+                <Copy fontType="H4" weight="bold">
+                  Feedback
+                </Copy>
+                <Copy fontType="Body2">
+                  Well done! you managed to save {} from last months budget. You
+                  can save up to 15% more if you target your spending on best
+                  value shopping deals :)
+                </Copy>
+                <HorizontalBar
+                    data={progressbar}
+                      options={{
+                        responsive: true,
+                        legend: {
+                          display: false
+                        },
+                        scales: {
+                          xAxes: [{
+                            stacked: true,
+                            display: false
+                          }],
+                          yAxes: [{
+                            stacked: true,
+                            display: false
+                          }]
+                        }
+                      }}
+                  />
+              </div>
+            </Col>
+          </Row>
+        <Row>
+          <Col span={24}>
+          <Copy fontType="H4" weight="bold">Expenditure</Copy>
+            <Line
+            width={100}
+            height={30}
+            data={lineData}
+            options={{
+              responsive: true,
+              legend: {
+                display: false
+              },
+              layout: {
+                padding: {
+                  left: 0,
+                  right: 0,
+                  top: 30,
+                  bottom: 50
+                }
+              },
+              scales: {
+                yAxes: [
+                  {
+                    gridLines: {
+                      display: true
+                    }
+                  }
+                ],
+                xAxes: [
+                  {
+                    gridLines: {
+                      display: false
+                    }
+                  }
+                ]
+              }
+            }}
+          />
+          </Col>
+        </Row>
+        <Row>
+            <Col span={12}>
+            <Copy fontType="H4" weight="bold">Top 5 Merchants</Copy>
+                <HorizontalBar data={horiData} options={{
+                  responsive: true,
+                  legend: {
+                    display: false
+                  },
+                  layout: {
+                    padding: {
+                      left: 0,
+                      right: 15,
+                      top: 30,
+                      bottom: 25
+                    }
+                  },
+                  scales: {
+                    yAxes: [
+                      {
+                        gridLines: {
+                          color: 'rgba(0, 0, 0, 0)'
+
+                        }
+                      }
+                    ],
+                    xAxes: [
+                      {
+                        gridLines: {
+                          color: 'rgba(0, 0, 0, 0)'
+                        }
+                      }
+                    ]
+                  }
+                }} />
+            </Col>
+            <Col span={12} >
+            <Copy style={{ paddingLeft: '30px' }} fontType="H4" weight="bold">Category Expenditure</Copy>
+            <Doughnut
+              data={donutData}
+              options={{
+                responsive: true,
+                title: {
+                  display: false
+                },
+                layout: {
+                  padding: {
+                    left: 0,
+                    right: 0,
+                    top: 30,
+                    bottom: 30
+                  }
+                },
+                legend: {
+                  display: true,
+                  position: 'right'
+                }
+              }}
+            />
+            </Col>
+        </Row>
+        <Row>
+          <Col span={24}>
+              <Bar
+              width={100}
+              height={30}
+              data={yearData}
+              options={{
+                layout: {
+                  padding: {
+                    left: 0,
+                    right: 0,
+                    top: 30,
+                    bottom: 30
+                  }
+                },
+                scales: {
+                  xAxes: [
+                    {
+                      gridLines: {
+                        drawBorder: false,
+                        color: 'rgba(0, 0, 0, 0)'
+                      }
+                    }
+                  ],
+                  yAxes: [{
+                    gridLines: {
+                      drawBorder: false,
+                      display: false
+                    },
+                    ticks: {
+                      beginAtZero: true,
+                      max: 5000
+                    }
+                  }]
+                }
+              }}/>
+          </Col>
+        </Row>
+      </div>
+    </Layout>
+  )
 }
