cbab4687dafe8d5f60000660f73e0246efb35e4a
Luliephi <lulamam@indiefin.com>
2020-11-18 19:40:56 +0200

    Added Three buttons underneath carasoul
    Buttons need further styling. Buttons have no funcionality.

--- src/components/VisaCard/VisaCard.js
+++ src/components/VisaCard/VisaCard.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 import { Row, Col } from 'antd';
+import { Button } from '@indiefin/galaxy-button'
 import Back from './back'
 import Next from './next'
 import Copy from '../Copy'
 import './style.css'
+
 
 
 const contentStyle = {
@@ -41,13 +43,32 @@ function VisaCard() {
                                 <div className='visa'>Visa</div>
                             </div>
                             </Slide>
-                            <Slide index={1}> <div className='slide'><Copy fontType="H2" weight="bold"> R2 300.00</Copy> </div></Slide>
-                            <Slide index={2}> <div className='slide'><Copy fontType="H2" weight="bold"> R5 000.00</Copy> </div></Slide>
+                            <Slide index={1}> 
+                            <div className='slide'>
+                                <div className='amount'><Copy fontType="H2" weight="bold"> R2 300.00</Copy></div> 
+                                <div className='cardNumber'>***6559</div>
+                                <div className='visa'>Visa</div>
+                            </div>
+                            </Slide>
+                            <Slide index={2}> 
+                            <div className='slide'>
+                                <div className='amount'><Copy fontType="H2" weight="bold"> R5 000.00</Copy></div>
+                                <div className='cardNumber'>***6559</div>
+                                <div className='visa'>Visa</div>
+                            </div>
+                            </Slide>
                         </Slider>
                     </Col>
                     <Col style={contentStyle} span={8}>
-                        <ButtonNext style={buttontStyle}><Next/></ButtonNext>
+                        <div><ButtonNext style={buttontStyle}><Next/></ButtonNext></div>
                     </Col>
+                </Row>
+                <Row>
+                    <Col span={6}></Col>
+                    <Col span={4}> <Button kind="fab"/></Col>
+                    <Col span={4}> <Button kind="fab"/></Col>
+                    <Col span={4}> <Button kind="fab"/></Col>
+                    <Col span={6}></Col>
                 </Row>
             </CarouselProvider>
         </div>
